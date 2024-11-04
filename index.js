b64571328664cd6bc651813132fa6a451680427d
Alle Manoj <22211a1205@bvrit.ac.in>
2024-11-04 11:53:49 +0530

    inputvalidation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { createTodo, updateTodo } = require('./types');
 const app = express();
 
 app.use(express.json());
@@ -10,13 +11,28 @@ app.use(express.json());
  * }
  */
 app.post("/todo",function(req,res){
-
+    const createdPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createdPayload);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg:"you send the wrong inputs"
+        })
+        return;
+    }
+    // create todo in mongoDb
 })
 app.get("todos",function(req,res){
 
 })
 app.put("/completed",function(req,res){
-
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg:"you messed up with the inputs"
+        })
+        return;
+    }
 })
 
 app.listen({

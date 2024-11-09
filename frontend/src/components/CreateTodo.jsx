9e3436513cc886500b0ab8d4b8908b7573c8d3db
Alle Manoj <22211a1205@bvrit.ac.in>
2024-11-09 22:28:36 +0530

    frontend add

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,8 +1,8 @@
 
 export function CreateTodo() {
     return <div>
-        <input type="text" placeholder="Todo Title" ></input>
-        <input type="text" placeholder="Description" ></input>
+        <input type="text" placeholder="Todo Title" ></input><br />
+        <input type="text" placeholder="Description" ></input><br />    
         
         <button>ADD A TODO</button>
     </div>
